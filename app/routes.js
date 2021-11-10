64db43abf0eda0d318fff78947bf739eb7bda83d
Gary Carr <imgarycarr@gmail.com>
2021-11-10 08:15:02 +0000

    Branching added

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,25 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+
+// MANAGE DATES ROUTING
+
+router.post('/manage-dates/add-date/add-date', function (req, res) {
+
+  // Make a variable and give it the value from 'how-many-balls'
+  var isMultiple = req.session.data['multiple-date']
+
+  // Check whether the variable matches a condition
+  if (isMultiple == "recurring-event"){
+    // Send user to recurring option
+    res.redirect('/manage-dates/add-date/recurring')
+  } else {
+    // Send user to experience type
+    res.redirect('/manage-dates/add-date/experience-type')
+  }
+
+})
+
+
+
 module.exports = router
