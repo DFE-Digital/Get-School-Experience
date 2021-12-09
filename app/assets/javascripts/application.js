f2f11390d8b0e7db54d100ad37e2eeecebdb8f1f
Gary Carr <imgarycarr@gmail.com>
2021-12-09 07:49:44 +0000

    select all functionality added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,3 +8,13 @@ if (window.console && window.console.info) {
 $(document).ready(function () {
   window.GOVUKFrontend.initAll()
 })
+
+
+//select all subjects (add/manage dates)
+
+document.getElementById('select-all').onclick = function() {
+  var checkboxes = document.getElementsByName('subject');
+  for (var checkbox of checkboxes) {
+    checkbox.checked = this.checked;
+  }
+}
